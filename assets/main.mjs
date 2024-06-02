d167f13fc8aa27fed71eb3d929a3f7ab53d6f924
Fabricio Silva <hi@fabricio.dev>
2024-06-02 16:16:05 +0100

    add handle click to select lines

--- assets/main.mjs
+++ assets/main.mjs
@@ -1,17 +1,70 @@
 import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js"
 import { decrypt } from "./subtle.mjs"
 
-/**
- * @param  {string}     contentType
- * @param  {Uint8Array} iv
- * @return {Promise<void>}
- */
-export const init = async (contentType, iv) => {
+const parseUrl = () => {
   const uuid = location.pathname.slice(1)
-  const [k, ...hash] = location.hash.slice(1).split(/(?=[:.])/)
+  const [k, ...rest] = location.hash.slice(1).split(/(?=[:.])/)
   // only if I could work with RegEx
-  const forcedLanguage = hash.find((str) => str.startsWith("."))?.slice(1)
-  const selectedLines = hash.find((str) => str.startsWith(":"))?.slice(1)
+  const forcedLanguage = rest.find((str) => str.startsWith("."))?.slice(1)
+  const selectedLines = rest.find((str) => str.startsWith(":"))?.slice(1)
+    .split("-").map((i) => parseInt(i) || 0) || []
+
+  return { uuid, k, forcedLanguage, selectedLines }
+}
+
+const addLineNumbers = (preElement) => {
+  const { height } = preElement.getClientRects()[0]
+  const { lineHeight } = getComputedStyle(preElement)
+  const totalLines = parseInt(height / parseFloat(lineHeight))
+  const listElement = document.createElement("ol")
+
+  for (let i = 1; i < totalLines; i++) {
+    const listItemElement = document.createElement("li")
+    listItemElement.dataset.ln = i
+    listElement.appendChild(listItemElement)
+  }
+
+  listElement.addEventListener("click", ({ target, shiftKey }) => {
+    const clicked = parseInt(target.dataset.ln)
+    const { k, forcedLanguage, selectedLines } = parseUrl()
+    let [from, to = from] = selectedLines
+
+    if (!clicked) {
+      return
+    }
+
+    // swap if clicking on prior lines
+    if (clicked < from) {
+      from = clicked
+    } else {
+      to = clicked
+    }
+
+    location.hash = `${k}${forcedLanguage ? `.${forcedLanguage}` : ""}:${shiftKey && from ? `${from}-${to}` : clicked}`
+  })
+
+  preElement.insertBefore(listElement, preElement.firstChild)
+}
+
+const selectLines = (preElement) => {
+  const [start, total = start] = parseUrl().selectedLines
+
+  if (!start) {
+    return
+  }
+
+  const markElement = document.querySelector("mark") || document.createElement("mark")
+  markElement.style.setProperty("--top", start - 1)
+  markElement.style.setProperty("--height", total - start + 1)
+
+  preElement.appendChild(markElement)
+
+  // scrollIntoView needs to defer
+  setTimeout(() => markElement.scrollIntoView(), 200)
+}
+
+export const init = async (contentType, iv) => {
+  const { uuid, k, forcedLanguage } = parseUrl()
   const preElement = document.createElement("pre")
   let childElement = document.createElement("code")
 
@@ -51,48 +104,6 @@ export const init = async (contentType, iv) => {
   }
 
   addLineNumbers(preElement)
-  selectLines(preElement, selectedLines)
-}
-
-/**
- * @param  {Element} preElement
- * @return {void}
- */
-const addLineNumbers = (preElement) => {
-  const { height } = preElement.getClientRects()[0]
-  const { lineHeight } = getComputedStyle(preElement)
-  const totalLines = parseInt(height / parseFloat(lineHeight))
-  const listElement = document.createElement("ol")
-
-  for (let i = 1; i < totalLines; i++) {
-    listElement.appendChild(
-      Object.assign(document.createElement("li"), {
-        id: `L${i}`,
-      }),
-    )
-  }
-
-  preElement.insertBefore(listElement, preElement.firstChild)
-}
-
-/**
- * @param  {Element} preElement
- * @param  {string} selectedLines
- * @return {void}
- */
-const selectLines = (preElement, selectedLines = "") => {
-  const [start, total = start] = selectedLines.split("-").map((i) => parseInt(i) || 0)
-
-  if (!start) {
-    return
-  }
-
-  const markElement = Object.assign(document.createElement("mark"))
-  markElement.style.setProperty("--top", start - 1)
-  markElement.style.setProperty("--height", total - start + 1)
-
-  preElement.appendChild(markElement)
-
-  // scrollIntoView needs to defer
-  setTimeout(() => markElement.scrollIntoView(), 100)
+  selectLines(preElement)
+  addEventListener("hashchange", () => selectLines(preElement))
 }

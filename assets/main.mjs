0605d960f888509a96f93a836d7393a6142bd038
Fabricio Silva <hi@fabricio.dev>
2024-06-02 11:00:23 +0100

    add selected lines feature

--- assets/main.mjs
+++ assets/main.mjs
@@ -8,7 +8,10 @@ import { decrypt } from "./subtle.mjs";
  */
 export const init = async (contentType, iv) => {
   const uuid = location.pathname.slice(1);
-  const [k, language] = location.hash.slice(1).split(".");
+  const [k, ...hash] = location.hash.slice(1).split(/(?=[#.])/);
+  // only if I could work with RegEx
+  const forcedLanguage = hash.find((str) => str.startsWith("."))?.slice(1);
+  const selectedLines = hash.find((str) => str.startsWith("#"))?.slice(1);
   const preElement = document.createElement("pre");
   let childElement = document.createElement("code");
 
@@ -38,8 +41,8 @@ export const init = async (contentType, iv) => {
     return;
   }
 
-  if (language) {
-    hljs.configure({ languages: [language] });
+  if (forcedLanguage) {
+    hljs.configure({ languages: [forcedLanguage] });
   }
 
   // do not call highlight.js if contents is too big
@@ -48,23 +51,47 @@ export const init = async (contentType, iv) => {
   }
 
   addLineNumbers(preElement);
+  selectLines(preElement, selectedLines);
 };
 
 /**
- * @param  {Element} blockElement
+ * @param  {Element} preElement
  * @return {void}
  */
-const addLineNumbers = (blockElement) => {
-  const { height } = blockElement.getClientRects()[0];
-  const { lineHeight } = getComputedStyle(blockElement);
+const addLineNumbers = (preElement) => {
+  const { height } = preElement.getClientRects()[0];
+  const { lineHeight } = getComputedStyle(preElement);
   const totalLines = parseInt(height / parseFloat(lineHeight));
   const listElement = document.createElement("ol");
 
   for (let i = 1; i < totalLines; i++) {
     listElement.appendChild(
-      Object.assign(document.createElement("li"), { id: `L${i}` }),
+      Object.assign(document.createElement("li"), {
+        id: `L${i}`,
+      }),
     );
   }
 
-  blockElement.insertBefore(listElement, blockElement.firstChild);
+  preElement.insertBefore(listElement, preElement.firstChild);
+};
+
+/**
+ * @param  {Element} preElement
+ * @param  {string} selectedLines
+ * @return {void}
+ */
+const selectLines = (preElement, selectedLines = "") => {
+  const [start, total = start] = selectedLines.split("-").map((i) =>
+    parseInt(i) || 0
+  );
+
+  if (!start) {
+    return;
+  }
+
+  const markElement = Object.assign(document.createElement("mark"));
+  markElement.style.setProperty("--top", start - 1);
+  markElement.style.setProperty("--height", total - start + 1);
+
+  preElement.appendChild(markElement);
 };

9b25ad05ad0d92b66890aaddb9b1a21c1b865e2e
Fabricio Silva <hi@fabricio.dev>
2024-12-31 14:23:31 +0000

    start koa server

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("hi");
+import Koa from "koa";
+import { BIND, PORT } from "./src/constants.js";
+
+const app = new Koa();
+
+app.listen(PORT, () => {
+  console.log(`Listen on http://${BIND}:${PORT}`);
+});
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT;
+export const BIND = process.env.BIND;
